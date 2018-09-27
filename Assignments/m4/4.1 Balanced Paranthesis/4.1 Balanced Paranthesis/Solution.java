6c7c2e4b4f4294e11f9c48d196c397ba907bade0
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-09-27 20:18:59 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- Assignments/m4/4.1 Balanced Paranthesis/4.1 Balanced Paranthesis/Solution.java
+++ Assignments/m4/4.1 Balanced Paranthesis/4.1 Balanced Paranthesis/Solution.java
@@ -1,27 +1,65 @@
 import java.util.Scanner;
+/**.
+ * Class for linked list stack of strings.
+ */
 class LinkedListStackOfStrings {
+    /**.
+     * { var_description }
+     */
 
+    /**.
+     * { var_description }
+     */
     private Node first = null;
+    /**.
+     * Class for node.
+     */
     private class Node {
+        /**.
+         * { var_description }
+         */
         char item;
         Node nextAddress;
     }
 
+    /**.
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
     public char top() {
+        /**.
+         * { item_description }
+         */
         return first.item;
     }
 
+    /**.
+     * Determines if empty.
+     *
+     * @return     True if empty, False otherwise.
+     */
     public boolean isEmpty() {
         return first == null;
     }
 
-    public void push(char item) {
+    /**.
+     * { function_description }
+     *
+     * @param      item  The item
+     */
+    public void push(final char item) {
         Node oldfirst = first;
         first = new Node();
         first.item = item;
         first.nextAddress = oldfirst;
     }
 
+    /**.
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
     public char pop() {
         char item = first.item;
         first = first.nextAddress;
@@ -29,8 +67,14 @@ class LinkedListStackOfStrings {
     }
 }
 
+/**.
+ * Class for solution.
+ */
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
+        /**.
+         * { var_description }
+         */
         Scanner sc = new Scanner(System.in);
         int size = Integer.parseInt(sc.nextLine());
         for (int i = 0; i < size; i++) {
@@ -43,7 +87,17 @@ public class Solution {
         }
     }
 
-    public static boolean checkingTheParanthesis(String s) {
+    /**.
+     * { function_description }
+     *
+     * @param      s     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static boolean checkingTheParanthesis(final String s) {
+        /**.
+         * { var_description }
+         */
         LinkedListStackOfStrings obj = new LinkedListStackOfStrings();
         int sLength = s.length();
         for (int i = 0; i < sLength; i++) {
@@ -52,15 +106,19 @@ public class Solution {
                 obj.push(ch);
             }
             else {
-                if(obj.isEmpty()) return false;
+                if (obj.isEmpty()) {
+                    return false;
+                }
                 if (ch == '}' && obj.top() == '{') {
                 obj.pop();
             } else if (ch == ']' && obj.top() == '[') {
                 obj.pop();
             } else if (ch == ')' && obj.top() == '(') {
                 obj.pop();
-            } else return false;
+            } else {
+            return false;
         }
+    }
 
     }
     return obj.isEmpty();
