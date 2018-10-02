c00da72270bfa86734c2fef498cc34af5840124e
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-10-02 21:21:56 +0530

    "check_style_score -> 6 of 6 passed. style score: 1/1 "

--- Assignments/m8/Assignment 1/8.1 Team Ranking With Insertion/8.1 Team Ranking With Insertion/Solution.java
+++ Assignments/m8/Assignment 1/8.1 Team Ranking With Insertion/8.1 Team Ranking With Insertion/Solution.java
@@ -21,7 +21,7 @@ public final class Solution {
  *
  * @param      args  The arguments
  */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Team[] cricketteams = new Team[TEN];
         int size = 0;
         Scanner scan = new Scanner(System.in);
