b9d4c444ce9d40e684a79d2c525fe51a082e3d3c
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-10-03 22:13:39 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

---
+++ Assignments/m9/9.1 Merge Sort Improvements/9.1 Merge Sort Improvements/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ final class Solution {
+    private Solution() {
+
+    }
+    public static void main(final String[] args) {
+        Mergesort merge = new Mergesort();
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String line = sc.nextLine();
+            String[] lines = line.split(",");
+            merge.sort(lines);
+            System.out.println(merge.display(lines));
+            System.out.println();
+        }
+    }
+ }
