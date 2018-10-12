a770268397c157cc684ac250b1c6d3737b0cf342
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-10-12 21:26:43 +0530

    "taxi_cab_numbers: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ Assignments/m17/TaxiCabNumbers/TaxiCabNumbers/CubeSum.java
@@ -0,0 +1,22 @@
+class CubeSum implements Comparable<CubeSum> {
+    long sum;
+    int i;
+    int j;
+
+    public CubeSum(int i, int j) {
+        this.sum = i * i * i + j * j * j;
+        this.i = i;
+        this.j = j;
+    }
+
+    public int compareTo(CubeSum that) {
+        if (this.sum < that.sum) return -1;
+        if (this.sum > that.sum) return +1;
+        return 0;
+    }
+
+    public String toString() {
+        return sum + "";
+    }
+
+}
